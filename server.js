5f995f19ec0b5af690bbad156ca0692c33f67904
HIENDQ <quyhien98@gmail.com>
2020-06-14 16:52:28 +0700

    init server

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+
+const Express = require('express')
+const multer = require('multer')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = Express()
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 5000;
+
+const Storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, './uploads')
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
+  },
+})
+
+const upload = multer({ storage: Storage })
+
+app.get('/', (req, res) => {
+
+     res.sendFile(path.join(`${__dirname}/uploads/photo_1592127650721_11.jpg`));
+    //return res.send('Done')
+
+})
+
+app.post('/api/upload', upload.array('photo', 3), (req, res) => {
+  let picture =[]
+  req.files.forEach(file => {
+    picture.push(`${__dirname}/uploads/${file.filename}`)
+  });
+  res.status(200).json({
+    message: 'success!',
+    picture
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`)
+})
