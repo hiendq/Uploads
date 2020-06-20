cb0147d3d4ae0e1c95409a260fee70b5bb34ec38
HIENDQ <quyhien98@gmail.com>
2020-06-21 01:28:19 +0700

    change cloudinary

--- server.js
+++ server.js
@@ -1,44 +1,51 @@
 require('dotenv').config();
 
-
 const Express = require('express')
 const multer = require('multer')
 const bodyParser = require('body-parser')
 const path = require('path')
+const Datauri = require('datauri/parser');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage }).array('photo');
+const cloudinary = require("cloudinary").v2;
+const meta = new Datauri();
 
 const app = Express()
 app.use(bodyParser.json())
 
+const dataUri = file => {
+  console.log("FILE NAME",file.originalname,path.extname(file.originalname))
+  return meta.format(path.extname(file.originalname).toString(), file.buffer);
+}
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const getFiles = (req)=>{
+  return req.files.map(file=>dataUri(file).content);
+}
+const uploadPhoto = async (file) => {
+  const result = await cloudinary.uploader.upload(file);
+  return result;
+};
 const port = process.env.PORT || 5000;
 
-const Storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, './uploads')
-  },
-  filename(req, file, callback) {
-    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
-  },
-})
-
-const upload = multer({ storage: Storage })
-
 app.get('/', (req, res) => {
-
-    // res.sendFile(path.join(`${__dirname}/uploads/photo_1592127650721_11.jpg`));
     return res.send('Done')
-
 })
-app.get('/api/get/:picture', (req, res) => {
-  const {picture} = req.params;
-  return res.sendFile(path.join(`${__dirname}/uploads/${picture}`));
-})
-
-app.post('/api/upload', upload.array('photo', 3), (req, res) => {
+app.post('/api/upload', upload, async(req, res) => {
   let picture =[]
-  req.files.forEach(file => {
-    picture.push(`https://my-picture-api.herokuapp.com/api/get/${file.filename}`)
-  });
-  res.status(200).json({
+  const files = getFiles(req);
+  for(let i in files){
+    const uploadResult = await uploadPhoto(files[i]);
+    picture.push(uploadResult.url);
+  }
+
+  res.status(201).json({
     message: 'success!',
     picture
   })
